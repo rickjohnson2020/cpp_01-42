627bbdc7b49d9fe3990bbdd9733b29bfc6d60b42
Riku Yano <riyano@student.42london.com>
2025-09-15 16:19:35 +0100

    modify to use pointers to member functions

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -2,6 +2,12 @@
 
 int main() {
 	Harl harl = Harl();
+
 	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	harl.complain("UNKNOWN");
+
 	return 0;
 }

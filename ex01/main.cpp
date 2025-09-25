aed0622b9a1d43bcc82c9dab98debc4a57d6f351
yano <eriksontokyo2020@gmail.com>
2025-09-25 14:31:16 +0100

    changed the name and number of zombies

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,8 +1,8 @@
 #include "./Zombie.hpp"
 
 int main() {
-	int N = 42;
-	Zombie* zombies = zombieHorde(N, "zombie");
+	int N = 4;
+	Zombie* zombies = zombieHorde(N, "yesname");
 	for (int i = 0; i < N; i++) {
 		zombies[i].announce();
 	}
